f5b6ad1a5178aa9a6abe2ac1f84270b85553256d
MatheusAmaralDeSousa <theusamarausyt@gmail.com>
2024-03-22 19:55:53 -0300

    merge: corrigindo merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,41 @@
 puts "Destroying existing records..."
 User.destroy_all
-Debt.destroy_all
 Person.destroy_all
-
-
+Debts.destroy_all
 User.create email: 'admin@admin.com', password: '111111'
 
 puts "Usuário criado:"
 puts "login admin@admin.com"
 puts "111111"
 
-1000.times do |counter|
-  puts "Creating user #{counter}"
-  User.create email: Faker::Internet.email, password: '111111'
+50.times do 
+    email = Faker::Internet.email
+    password = Faker::Internet.password
+    created_user_time = Faker::Time.between(from: 2.years.ago, to: Time.current)
+
+    user = User.create(email: email, password: password, created_at: created_user_time, updated_at: created_user_time)
+    puts "Usuário criado:"
+    puts "login #{email}"
+    puts "#{password}"
+    puts "Criado em: #{user.created_at}"
 end
 
-3000.times do |counter|
-  puts "Inserting Person #{counter}" 
-
-  attrs = {
-    name: Faker::Name.name,
-    phone_number: Faker::PhoneNumber.phone_number,
-    national_id: CPF.generate,
-    active: [true, false].sample,
-    user: User.order('random()').first
-  }
-  person = Person.create(attrs)
-
-  5.times do |debt_counter|
-    puts "Inserting Debt #{debt_counter}"
-    person.debts.create(
-      amount: Faker::Number.between(from: 1, to: 200),
-      observation: Faker::Lorem.paragraph
+100.times do
+    Person.create(
+        name: Faker:Name.name,
+        phone_number: Faker:: PhoneNumber.phone_number,
+        national_id: Faker:: IDNumber.brazilian_citizen_number,
+        active: Faker:: Boolean.boolean,
+        User: User.all.sample
     )
-  end
+end
+
+500.times do
+    Debts.create!(
+        Person: Person.all.sample,
+        amount: Faker::Number.decimal(l_digits: 2),
+        observation: Faker:: Lorem.setence,
+        created_at: Faker::Time.between(from: 2.years.ago, to: Time.current),
+        updated_at: Faker::Time.between(from: 2.years.ago, to: Time.current)
+    )
 end
