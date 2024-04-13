5bc96e95fc92febda96c19dd1d95626362dc4d62
Matheus Amaral De Sousa <theusamarausyt@gmail.com>
2024-04-12 22:11:25 -0300

    feat: Add balance

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,16 @@
 class Person < ApplicationRecord
   belongs_to :user, optional: true
 
+  before_save :update_amount
+
   has_many :debts, dependent: :destroy
+  has_many :payments, dependent: :destroy
 
   validates :name, :national_id, presence: true
   validates :national_id, uniqueness: true
   validate :cpf_or_cnpj
+  validate :valida_valor 
+  after_save :amount
 
   # TODO: refactor me
   #
@@ -13,20 +18,27 @@ class Person < ApplicationRecord
   # - sum payments
   # - rename to "balance"
   def total_debts
-    total = 0
+    debts_total = debts.sum(:amount)
+    payments_total = payments.sum(:amount)
 
-    debts.each do |debt|
-      total -= debt.amount
-    end
-
-    total
+    debts_total -= payments_total
   end
 
+  def update_amount
+    amount = total_debts
+    !save
+  end
   private
+
 
   def cpf_or_cnpj
     if !CPF.valid?(national_id) && !CNPJ.valid?(national_id)
       errors.add :national_id, :invalid
+    end
+  end
+
+  def valida_valor
+    if payments.nil?
     end
   end
 end
